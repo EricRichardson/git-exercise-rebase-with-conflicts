8f98898e59d38547bba45e0c75a6ea5fda1f97a6
Tammam Kbeili <tammam.kbeili@gmail.com>
2016-04-29 08:19:20 -0700

    refactor to use constants for calorie_count instead of magic numbers

--- cookie.rb
+++ cookie.rb
@@ -3,12 +3,15 @@ class Cookie
   attr_accessor :flour
   attr_accessor :butter
 
+
   def initialize(sugar, flour, butter)
     @sugar, @flour, @butter = sugar, flour, butter
+    SUGAR_CALORIES = 3.87
+    FLOUR_CALORIES = 3.64
   end
 
   def calorie_count
-    @sugar * 3.87 + @flour * 3.64
+    @sugar * SUGAR_CALORIES + @flour * FLOUR_CALORIES
   end
 
 end
